f28fc22f880544188ac60d3c471cf23cabc6ec4a
hdung1091 <hdung1091@gmail.com>
2021-04-17 21:45:28 +0700

    pipeline 2

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 
 app.get('/', function(req, res){
-    res.send('hello world Pipeline 1'); 
+    res.send('hello world Pipeline 2'); 
 });
 
 // Only works on 3000 regardless of what I set environment port to or how I set [value] in app.set('port', [value]).
