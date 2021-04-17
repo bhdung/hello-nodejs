3833f3526c41836ac8eccc160b94915f3604bb3c
hdung1091 <hdung1091@gmail.com>
2021-04-17 22:23:21 +0700

    cleanup on host

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 
 app.get('/', function(req, res){
-    res.send('hello world Pipeline 4'); 
+    res.send('hello world Pipeline 6, cleanup images on host'); 
 });
 
 // Only works on 3000 regardless of what I set environment port to or how I set [value] in app.set('port', [value]).
