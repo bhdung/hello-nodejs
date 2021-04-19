90d52dc615c302ee701ac9cbb42ea6857a27bdee
hdung1091 <hdung1091@gmail.com>
2021-04-19 15:07:26 +0700

    Change app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 
 app.get('/', function(req, res){
-    res.send('Pipeline Jenkins file. 1'); 
+    res.send('Pipeline Jenkins file. 2'); 
 });
 
 // Only works on 3000 regardless of what I set environment port to or how I set [value] in app.set('port', [value]).
