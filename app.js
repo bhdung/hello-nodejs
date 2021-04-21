b7bc253ca8e6468f10e17a75f124f77538a07d93
hdung1091 <hdung1091@gmail.com>
2021-04-22 02:15:54 +0700

    abcd

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 
 app.get('/', function(req, res){
-    res.send('Pipeline Jenkins file. Pull latest images, Deploy over ssh in Server Deploy'); 
+    res.send('Pipeline Jenkins file. Pull latest images, Deploy over ssh in Server Deploy...'); 
 });
 
 // Only works on 3000 regardless of what I set environment port to or how I set [value] in app.set('port', [value]).
