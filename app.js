4c00ab65079dca780f6cec57da99e0f858160112
hdung1091 <hdung1091@gmail.com>
2021-04-22 14:30:44 +0700

    abc

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ var express = require("express");
 var app = express();
 
 app.get('/', function(req, res){
-    res.send('Deploy over ssh in Server Deploy..'); 
+    res.send('Demo Nodejs Express'); 
 });
 
+app.get('/about', function(req, res){
+    res.send('Jenkins. Deploy over ssh in Server Deploy..'); 
+});
 // Only works on 3000 regardless of what I set environment port to or how I set [value] in app.set('port', [value]).
 app.listen(3000);
 // Test change commend
